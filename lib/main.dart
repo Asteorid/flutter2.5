fc48e1c06b6e7112995a8cadab31bd283d9b6283
Asteorid <taipovadil@gmail.com>
2024-01-26 21:27:45 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key, Key? key});
+  const MyApp({super.key, Key});
 
   @override
   Widget build(BuildContext context) {
@@ -28,63 +28,120 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.blue,
         ),
         body: Center(
-          child: Container(
-            color: Colors.blue,
-            height: 500,
-            width: 250,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red,
-                      fixedSize: const Size(100, 100),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20))),
-                  child: const Text(
-                    'Button',
-                    style: TextStyle(color: Colors.white),
-                  ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    fixedSize: const Size(100, 100),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
+                child: const Text(
+                  'Button',
+                  style: TextStyle(color: Colors.white),
                 ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    minimumSize: const Size(100, 50),
-                  ),
-                  child: const Text(
-                    'Button',
-                    style: TextStyle(color: Colors.white),
-                  ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  minimumSize: const Size(100, 50),
                 ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    minimumSize: const Size(100, 50),
-                  ),
-                  child: const Text(
-                    'Button',
-                    style: TextStyle(color: Colors.white),
-                  ),
+                child: const Text(
+                  'Button',
+                  style: TextStyle(color: Colors.white),
                 ),
-                ElevatedButton.icon(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                  ),
-                  icon: const Icon(
-                    Icons.add_to_drive_rounded,
-                    color: Colors.white,
-                  ),
-                  label: const Text(
-                    'Data',
-                    style: TextStyle(color: Colors.white),
-                  ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  minimumSize: const Size(100, 50),
                 ),
-              ],
-            ),
+                child: const Text(
+                  'Button',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              ElevatedButton.icon(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                icon: const Icon(
+                  Icons.add_to_drive_rounded,
+                  color: Colors.white,
+                ),
+                label: const Text(
+                  'Data',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 70),
+                child: Row(
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        minimumSize: const Size(100, 50),
+                      ),
+                      child: const Text(
+                        'Button',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 50,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'Hello world',
+                          style: TextStyle(color: Colors.black, fontSize: 16),
+                        ),
+                        Text(
+                          'World hello',
+                          style: TextStyle(color: Colors.black, fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(40),
+                child: Text(
+                  'New text for flutter project, less words more work',
+                  style: TextStyle(
+                      backgroundColor: Colors.yellow,
+                      color: Colors.purple,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(50),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Icon(
+                      Icons.volume_up,
+                      color: Colors.black,
+                      size: 70,
+                    ),
+                    Text(
+                      'text speaker',
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
